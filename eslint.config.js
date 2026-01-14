171a6dc7dfbc3f34bbe3c0c951c20673ad4e1bf7
Yash Joshi <yashjoshi4755@gmail.com>
2026-01-14 09:44:21 +0530

    Configured babel, prettier, tailwindcss (nativewind) for the project and linted and formatted the codebase accordingly.

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,18 @@
-// https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = defineConfig([
   expoConfig,
+  eslintPluginPrettierRecommended,
   {
     ignores: ['dist/*'],
+  },
+  ,
+  {
+    rules: {
+      // This ensures ESLint doesn't fight Prettier's formatting
+      'prettier/prettier': ['error', { trailingComma: 'all' }],
+    },
   },
 ]);
