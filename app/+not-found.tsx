e97c412ec8f832c330e6673252bcd6dbcb586c75
Yash Joshi <yashjoshi4755@gmail.com>
2026-01-17 08:56:18 +0530

    Deleted unnessary files and updated asset paths for icons and images.

---
+++ app/+not-found.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function NotFoundScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>404 - Page Not Found</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
