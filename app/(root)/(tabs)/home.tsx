9a943a3abe2904aa92154903e7a207d7e6f96138
Yash Joshi <yashjoshi4755@gmail.com>
2026-02-05 08:00:42 +0530

    removed env

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,35 +1,15 @@
-import { SignedIn, useClerk, useUser } from '@clerk/clerk-expo';
-import { useRouter } from 'expo-router';
-import { Text, TouchableOpacity } from 'react-native';
+import { SignedIn, useUser } from '@clerk/clerk-expo';
+import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function Page() {
+export default function Home() {
   const { user } = useUser();
-
-  // Use `useClerk()` to access the `signOut()` function
-  const { signOut } = useClerk();
-  const router = useRouter();
-
-  const handleSignOut = async () => {
-    try {
-      await signOut();
-      // Redirect to your desired page
-      router.replace('/');
-    } catch (err) {
-      // See https://clerk.com/docs/guides/development/custom-flows/error-handling
-      // for more info on error handling
-      console.error(JSON.stringify(err, null, 2));
-    }
-  };
 
   return (
     <SafeAreaView>
       <SignedIn>
         <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
       </SignedIn>
-      <TouchableOpacity onPress={handleSignOut}>
-        <Text>Sign out</Text>
-      </TouchableOpacity>
     </SafeAreaView>
   );
 }
